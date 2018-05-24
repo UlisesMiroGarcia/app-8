620e3fa9facd5b47978da569b91a3bc63102bab3
UlisesMiroGarcia <ulig244@gmail.com>
2018-05-23 21:57:12 -0500

    1subida
    Octava Aplicación

---
+++ js/acciones(app5).js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		$('#sonar').tap(function(){
+			navigator.notification.beep(2);
+		}); //Cierre del beep
+		$('#vibrar').tap(function(){
+			navigator.notification.vibrate(2000);
+		}); //Cierre de vibración
+	},false); //Cierre  del deviceready
+}); //Cierre del ready
